9db591b6ed77a29c6cb8a8322fc417e214c8f93c
natasha-gri <natashagri.dw@gmail.com>
2022-11-01 16:59:15 +0500

    1.11.22-1.Блок слайдера

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 'use strict';
 
+const swiperClients1 = new Swiper('.v-clients-slider', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    // пауза между прокрутками
+    delay: 0,
+    // отключить после ручного переключения
+    disableOnInteraction: false
+  },
+  speed: 105000,
+  spaceBetween: 10,
+  slidesPerView: 5, 
+  grabCursor: false,
+  slidesPerGroup: 20,
+  simulateTouch: false,
+});
 
-
+const swiperlients2 = new Swiper('.v2-clients-slider', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    // пауза между прокрутками
+    delay: 0,
+    // отключить после ручного переключения
+    disableOnInteraction: false
+  },
+  speed: 150000,
+  spaceBetween: 10,
+  slidesPerView: 5.7, 
+  grabCursor: false,
+  slidesPerGroup: 15,
+});
