3675379d411a97f69ac198f7a6b9840ccffcaebc
natasha-gri <natashagri.dw@gmail.com>
2022-11-02 15:05:52 +0500

    2.11.22-1.Предварительно закончила верстку главной

--- js/main.js
+++ js/main.js
@@ -4,32 +4,29 @@ const swiperClients1 = new Swiper('.v-clients-slider', {
   direction: 'horizontal',
   loop: true,
   autoplay: {
-    // пауза между прокрутками
-    delay: 0,
-    // отключить после ручного переключения
-    disableOnInteraction: false
+  delay: 0,
+  disableOnInteraction: false
   },
   speed: 105000,
   spaceBetween: 10,
-  slidesPerView: 5, 
+  slidesPerView: 5,
   grabCursor: false,
   slidesPerGroup: 20,
   simulateTouch: false,
-});
-
-const swiperlients2 = new Swiper('.v2-clients-slider', {
+  
+  });
+  
+  const swiperClients2 = new Swiper('.v2-clients-slider', {
   direction: 'horizontal',
   loop: true,
   autoplay: {
-    // пауза между прокрутками
-    delay: 0,
-    // отключить после ручного переключения
-    disableOnInteraction: false
+  delay: 0,
+  disableOnInteraction: false
   },
   speed: 110000,
   spaceBetween: 10,
-  slidesPerView: 6.7, 
+  slidesPerView: 7,
   grabCursor: false,
-  slidesPerGroup: 15,
+  slidesPerGroup: 20,
   simulateTouch: false,
-});
+  });
