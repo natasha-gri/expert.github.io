d9f24b7dc1e1a9757491f405132ca4fdd87064f0
natasha-gri <natashagri.dw@gmail.com>
2022-11-03 14:24:04 +0500

    3.11.22-2.Запустила слайдер команды

--- js/main.js
+++ js/main.js
@@ -16,17 +16,36 @@ const swiperClients1 = new Swiper('.v-clients-slider', {
   
   });
   
-  const swiperClients2 = new Swiper('.v2-clients-slider', {
+const swiperClients2 = new Swiper('.v2-clients-slider', {
+direction: 'horizontal',
+loop: true,
+autoplay: {
+delay: 0,
+disableOnInteraction: false
+},
+speed: 110000,
+spaceBetween: 10,
+slidesPerView: 7,
+grabCursor: false,
+slidesPerGroup: 20,
+simulateTouch: false,
+});
+
+const swiperTeam = new Swiper('.v-team-slider', {
   direction: 'horizontal',
   loop: true,
-  autoplay: {
-  delay: 0,
-  disableOnInteraction: false
+    autoplay: {
+    delay: 2000,
+     
   },
-  speed: 110000,
-  spaceBetween: 10,
-  slidesPerView: 7,
-  grabCursor: false,
-  slidesPerGroup: 20,
-  simulateTouch: false,
-  });
+  speed: 1000,
+  spaceBetween: 30,
+  slidesPerView: 3,
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+    
+});
